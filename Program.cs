970bb9483c575f24ea1cf7dc833486e82ba74b16
Nils <nils@piksel.se>
2012-04-23 12:19:20 +0200

    initial git upload

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Drawing;
+
+namespace forever
+{
+    class Program
+    {
+        public static volatile bool stop = false;
+        public static volatile bool done = false;
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Usage();
+                return;
+            }
+
+            string fileName = args[0];
+            string fileArgs = "";
+            for(int i=1;i<args.Length;i++)
+                fileArgs += " "+args[i];
+
+            StartAndWatch(fileName, fileArgs);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            SysTrayContext stContext = new SysTrayContext();
+            Application.Run(stContext);
+
+        }
+
+        static void Usage()
+        {
+            MessageBox.Show(
+                "usage:\n\nforever <application>",
+                "forever usage",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1                
+            );
+        }
+
+        static void StartAndWatch(string fileName, string fileArgs)
+        {
+            new Thread(delegate()
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = fileName,
+                    Arguments = fileArgs
+                };
+
+                while (true)
+                {
+                    if (stop) {
+                        done = true;
+                        break;
+                    }
+                    try
+                    {
+                        if (System.Environment.HasShutdownStarted) break;
+                        var p = Process.Start(psi);
+                        p.WaitForExit();
+                    }
+                    catch(Exception x)
+                    {
+                        MessageBox.Show(
+                            String.Format("Error while running arguments.\n\nError:\n  {0}\nFilename:\n  {1}\nArguments:\n  {2}", x.Message, fileName, fileArgs),
+                            "Error while running arguments",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    }
+                }
+            }).Start();
+        }
+    }
+}
---
+++ TrayIconContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace forever
+{
+    class SysTrayContext: ApplicationContext
+    {
+        private System.ComponentModel.IContainer mComponents;
+        private NotifyIcon mNotifyIcon;
+        private ContextMenuStrip mContextMenu;
+        private ToolStripMenuItem mDisplayForm;
+        private ToolStripMenuItem mExitApplication;
+        private Program program;
+
+        public SysTrayContext()
+        {
+            //Instantiate the component Module to hold everything
+            mComponents = new System.ComponentModel.Container();
+    
+    
+            //Instantiate the NotifyIcon attaching it to the components container and 
+            //provide it an icon, note, you can imbed this resource 
+            mNotifyIcon = new NotifyIcon(this.mComponents);
+            mNotifyIcon.Icon = forever.icon;
+
+            mNotifyIcon.Text = "Forever";
+            mNotifyIcon.Visible = true;
+
+            //Instantiate the context menu and items
+            mContextMenu = new ContextMenuStrip();
+            mDisplayForm = new ToolStripMenuItem();
+            mExitApplication = new ToolStripMenuItem();
+
+            //Attach the menu to the notify icon
+            mNotifyIcon.ContextMenuStrip = mContextMenu;
+
+            mExitApplication.Text = "Exit";
+            mExitApplication.Click += new EventHandler(mExitApplication_Click);
+            mContextMenu.Items.Add(mExitApplication);
+        }
+
+        void mExitApplication_Click(object sender, EventArgs e)
+        {
+            //Call our overridden exit thread core method!
+            ExitThreadCore();
+        }
+
+        protected override void ExitThreadCore()
+        {
+            //Clean up any references needed
+            //At this time we do not have any
+            //
+            Program.stop = true;
+            //while(Program.done
+
+            //Call the base method to exit the application
+            base.ExitThreadCore();
+        }
+    }
+}
